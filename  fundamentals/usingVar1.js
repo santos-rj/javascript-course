5b140b45f53442ae1727710f8ca1919d1fb78ea2
Rafael Santos <raf535@outlook.com>
2020-12-26 17:53:26 -0300

    Var exercises

---
+++  fundamentals/usingVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var will = 'Será???'
+            }
+        }
+    }
+}
+
+console.log(will)
+
+function test() {
+    var local = 123
+    console.log(local)
+}
+
+test()
+console.log(local)
