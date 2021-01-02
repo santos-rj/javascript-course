ddafee990409a7b53037b6da05732ad011fe6bba
Rafael Santos <raf535@outlook.com>
2021-01-02 16:21:31 -0300

    Returns and parameters

---
+++ functions/returnParametersOptional.js
@@ -0,0 +1,15 @@
+function area(width, height) {
+    const area = width * height
+
+    if (area > 20) {
+        console.log(`Valor acima do permitido: ${area}m².`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2, 2))
+console.log(area(2))
+console.log(area())
+console.log(area(2, 2, 17))
+console.log(area(5,5))
