f8937cff7c6342deab6cfaf56a4125537fafd547
Rafael Santos <raf535@outlook.com>
2021-02-06 11:38:52 -0300

    Call and Apply

---
+++ functions/callApply.js
@@ -0,0 +1,27 @@
+function getPrice(tax = 0, currency = 'R$') {
+    return `${currency} ${this.price * (1 - this.desc) * (1 + tax)}`
+}
+
+const product = {
+    name: 'Notebook',
+    price: 4589,
+    desc: 0.15,
+    getPrice
+}
+
+global.price = 20
+global.desc = 0.1
+console.log(getPrice())
+
+console.log(product.getPrice())
+
+const car = {
+    price: 49990,
+    desc: 0.20
+}
+
+console.log(getPrice.call(car))
+console.log(getPrice.apply(car))
+
+console.log(getPrice.call(car, 0.17, '$'))
+console.log(getPrice.apply(car, [0.17, '$']))
